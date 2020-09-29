eb3e80bab2a9ad57fbb1c05322dfa06cb1318707
Ismail Shuaau <you@example.com>
2020-09-29 19:29:00 +0500

    code fomatted

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -14,13 +14,15 @@
         <button class="color-trans" onclick="menuClose()">
             <div>
                 <span class="side-nav-arrow"> <i class="fas fa-chevron-left arrow-light"></i> </span>
-                <span class="side-nav-book "> <i class="far fa-bookmark bookmark"></i><span class="side-nav-text">Saved Reports</span></span>
+                <span class="side-nav-book "> <i class="far fa-bookmark bookmark"></i><span class="side-nav-text">Saved
+                        Reports</span></span>
             </div>
         </button>
     </div>
     <div class="list-container">
         <!-- Accordin Start -->
-        <button id="accordinToggle" class="button w3-block w3-left-align list-close" onclick="openAccordin(event)">my Reports
+        <button id="accordinToggle" class="button w3-block w3-left-align list-close" onclick="openAccordin(event)">my
+            Reports
             <i id="accordin-arrow" class="fas fa-chevron-right arrow-light"></i>
         </button>
         <div id="reportAccord" class="w3-bar-block w3-hide w3-white">
@@ -55,17 +57,19 @@
                     <div>
                         <div id="title-field0" class="item-container" style="display:none;">
                             <i class="fas fa-rocket color-valencia"></i>
-                            <span class="item"><input id="save" type="text" name="title" value="" data-id="0" autofocus></span>
+                            <span class="item"><input id="save" type="text" name="title" value="" data-id="0"
+                                    autofocus></span>
                         </div>
                     </div>
                     <div class="card">
                         <div class="save-container">
                             <i class="fas fa-plus-circle save"></i>
-                            <button onclick="saveReport(event)" class="save" data-id="0" type="submit" id="ajaxSave" value="save">Save Report</button>
+                            <button onclick="saveReport(event)" class="save" data-id="0" type="submit" id="ajaxSave"
+                                value="save">Save Report</button>
                         </div>
                     </div>
+                </form>
             </div>
-            </form>
         </div>
         <!-- END / Add Report -->
     </div>
@@ -77,7 +81,8 @@
         <ul>
             <li>
                 <a href="#bookmark">
-                    <button id="openNav" class="teal border-grey nav-button" onclick="menuOpen()"><i class="far fa-bookmark light-gray"></i></button>
+                    <button id="openNav" class="teal border-grey nav-button" onclick="menuOpen()"><i
+                            class="far fa-bookmark light-gray"></i></button>
                 </a>
             </li>
             <li><a href="#users"></a></li>
